9e275151e1f604308e2e98399b010b8e9cf8f00a
IfritSorcerer <ianisern@yahoo.com>
2024-05-17 12:04:53 -0400

    Initial File Cleanup, Added initial file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AboutMe } from "./containers/aboutMe/AboutMe";
+import { Header } from "./containers/header/Header";
+import { Footer } from "./containers/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AboutMe />
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
